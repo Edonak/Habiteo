5a8f04d8024a3a725c860a6a93e6f613813740d7
Edonak <edouardnakasila69@gmail.com>
2024-04-18 16:44:18 +0100

    developpement header bar de navigation fini

--- frontend/src/component/header/navbarLink.jsx
+++ frontend/src/component/header/navbarLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function NavbarLink() {
     return (
-        <ul className='flex justify-between gap-10 text-orange-500 font-semibold'>
+        <ul className='flex justify-between gap-10 text-orange-500 items-center mt-6 pr-6 font-semibold'>
             <li to='/rent'>Louer</li>
             <li to='/sell'>Vendre</li>
             <li to='/buy'>Acheter</li>

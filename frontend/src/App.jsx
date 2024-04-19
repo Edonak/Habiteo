f9762ca33ad09021504417d527fd37e5ce2817af
Edonak <edouardnakasila69@gmail.com>
2024-04-19 05:06:46 +0100

    mis en place de la hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './component/header/navbar'
+import HeroSection from './component/heroSection/heroSection'
 
 function App() {
   return (
     <>
     <Navbar />
+    <HeroSection />
     </>
   )
 }

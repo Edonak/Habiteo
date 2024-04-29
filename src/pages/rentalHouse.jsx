54b4f54aa00b3ffdb7086854fa5340689d1f88dd
Edonak <edouardnakasila69@gmail.com>
2024-04-29 09:52:27 +0100

    ajout du composant primaire rentAndSellHouse

--- src/pages/rentalHouse.jsx
+++ src/pages/rentalHouse.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Navbar from '../component/header/navbar';
 import Footer from '../component/footer/footer';
+import RentAndSellHouse from '../component/primaryComponents/rentAndSellHouse';
 
 export default function RentalHouse() {
+  
+
     return (
         <section>
-                <Navbar />
-            
-            
-
-                <Footer />
+            <Navbar />
+            <main>
+                <RentAndSellHouse />
+                <section></section>
+            </main>
+            <Footer />
         </section>
     );
 }
